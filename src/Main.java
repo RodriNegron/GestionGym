67f93110868b64f5ba94cfed8c3d23cc307b8e8f
alecasella <56683589+alecasella@users.noreply.github.com>
2021-05-29 17:45:41 -0300

    function menu in main to rebuild

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         /*
-        Customer cust1 = new Customer("38606789", "Alessandro", "Casella", "javi_casella95@outlook.com.ar", "Qwer1379", 0);
+        Customer cust1 = new Customer("38606789", "Alessandro", "Casella", "javi_casella95@outlook.com.ar", "Qwer1379");
 
         Shift shift1 = new Shift("24/02/71995", "15:30", "Musculacion");
 
@@ -51,12 +51,14 @@ public class Main {
 
             switch (number) {
                 case 1:
+                   // menu(VS);
 
                     break;
 
                 case 2:
                     cust = gym.register(scann);
                     gym.addToCustomerList(cust);
+                    gym.consultClients();
                     break;
 
                 default:
@@ -71,4 +73,83 @@ public class Main {
     }
 
 
+    /*
+    public static void menu(VideoStore VS) {
+        int number;
+        char var = 's';
+
+        Cliente cli;
+        cli = VS.consultaYRetornaCliente();
+
+        if (cli != null) {
+            if (cli.getNombre() != "admin") {
+                do {
+                    System.out.println("Bienvenido " + cli.getNombre() + "!:D");
+                    System.out.println("1-Alquilar pelicula");
+                    System.out.println("2-Devolver pelicula");
+                    System.out.println("3-Consultar peliculas alquiladas");
+                    System.out.println("4-Consultar por titulo/genero"); //TODO
+
+                    number = globalScann.nextInt();
+
+                    switch (number) {
+                        case 1:
+                            VS.alquilarPelicula(cli);
+                            break;
+                        case 2:
+                            VS.devolverPelicula(cli);
+                            break;
+                        case 3:
+                            ListadoBoletas auxiliar = VS.consultaPeliculasAlquiladas(cli);
+                            if (cli.getTickets().retornarCantEnAlquilerCliente() == 0)
+                                System.out.println("No se encontraron peliculas en alquiler");
+                            else
+                                cli.getTickets().consultarListadoBoletas();
+                            break;
+                        default:
+                            System.out.println("Usted ha intentado consultar un valor erroneo");
+                    }
+                    System.out.println("Desea continuar operando? " + cli.getNombre() + "?" + "s/n");
+                    globalScann.nextLine();
+                    var = globalScann.nextLine().charAt(0);
+                } while (var == 's');
+            }
+            else {
+                do {
+                    System.out.println("Bienvenido " + cli.getNombre() + "!:D");
+                    System.out.println("1-Consultar alquileres vigentes");
+                    System.out.println("2-Devoluciones del dia");
+                    System.out.println("3-Consultar ultimos alquileres de un cliente");
+                    System.out.println("4-Titulos mas alquilados");
+                    System.out.println("5-Mostrar clientes registrados");
+                    System.out.println("Elija una opcion: ");
+                    number = globalScann.nextInt();
+                    switch (number) {
+                        case 1:
+                            System.out.println(VS.consultarAlquileresVigentes());
+                            break;
+                        case 2:
+                            System.out.println(VS.consultarDevolucionesDiaDeHoy());
+                            break;
+                        case 3:
+                            System.out.println(VS.consultarUltimosDiezAlq());
+                            break;
+                        case 4:
+                            VS.consultarPeliculaMasPopular();
+                            break;
+                        case 5:
+                            VS.consultarClientesRegistrados();
+                            break;
+                        default:
+                            System.out.println("Usted ha intentado consultar un valor erroneo");
+                    }
+                    System.out.println("Desea continuar operando? " + cli.getNombre() + "?" + "s/n");
+                    globalScann.nextLine();
+                    var = globalScann.nextLine().charAt(0);
+                } while (var == 's');
+            }
+        }else   System.out.println("Cliente no registrado!");
+    }
+      */
 }
+
