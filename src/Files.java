6c3cc2d73972fcb2ae4f3507b54b0126410a4856
RODRIGO NEGRON <rodrigoo.n@hotmail.com.argit config --global user.email rodrigoo.n@hotmail.com.ar>
2021-05-31 20:29:47 -0300

    user write / read method created at files.java

--- src/Files.java
+++ src/Files.java
@@ -1,1 +1,59 @@
 
+import Classes.Customer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Files {
+    private static final String Customer_file = "customers.json";
+    private static Gson gson ;
+
+    private Files(){
+    }
+
+    private static Gson getGson() {
+        return gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    public ArrayList<Customer> readCustomerFile () {
+        ArrayList<Customer> customers = null;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new BufferedReader(new FileReader(Customer_file)));
+            customers = getGson().fromJson(reader, (new TypeToken<ArrayList<Customer>>() {
+            }.getType()));
+        } catch (IOException e) {
+            customers = new ArrayList<>();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return customers;
+    }
+
+    public void writeCustomersFile( ArrayList<Customer> customers){
+        BufferedWriter writer = null;
+        try{
+            writer = new BufferedWriter(new FileWriter(Customer_file));
+            getGson().toJson(customers, customers.getClass(), writer);
+        } catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(writer != null){
+                try{
+                    writer.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
